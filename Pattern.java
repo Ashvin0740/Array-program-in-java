a3aef9577504b1b563ac74d9adc07b82365b294e
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-07-27 19:36:35 +0530

    simple pattern
    simple java program to print the pattern style

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+class pattern
+{
+    public static void main(String []args){
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * * * *");
+                                               
+         
+         
+
+
+    }
+}
